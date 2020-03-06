dff08937800c51711ddc4706c49c1fa3bbfc2f8e
Cristiano Carvalho Amaral <cacofut@hotmail.com>
2020-03-05 21:52:38 -0300

    RestAPI com link

--- src/main/java/br/com/salao/api/IngredientControllerRest.java
+++ src/main/java/br/com/salao/api/IngredientControllerRest.java
@@ -2,6 +2,7 @@ package br.com.salao.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,10 +32,18 @@ public class IngredientControllerRest {
 	}
 	
 	@GetMapping
-	public CollectionModel<IngredientModel> allIngredients(){
+	public ResponseEntity<CollectionModel<IngredientModel>> allIngredients(){		
 		Iterable<Ingredient> ingredients = ingredientRepo.findAll();
-		return ingredientAssembler.toCollectionModel(ingredients);		
-	} 
+		return new ResponseEntity<>(ingredientAssembler.toCollectionModel(ingredients), HttpStatus.OK);		
+	}
+	
+	/*@GetMapping
+	public ResponseEntity<List<Ingredient>> allIngredients(){		
+		Iterable<Ingredient> ingredients = ingredientRepo.findAll();
+		List<Ingredient> teste = new ArrayList<>();
+		ingredients.forEach(teste::add);		
+		return new ResponseEntity<>(teste, HttpStatus.OK);		
+	}*/ 
 	
 	@GetMapping("{id}")	
 	public ResponseEntity<IngredientModel> ingredientById(@PathVariable Long id){		
--- src/test/java/br/com/salao/IngredientControllerRestTest.java
+++ src/test/java/br/com/salao/IngredientControllerRestTest.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import br.com.salao.entity.Ingredient;
+import br.com.salao.resource.IngredientModel;
 
 
 
@@ -18,7 +19,7 @@ class IngredientControllerRestTest {
 	int randomServerPort;
 	
 	@Test
-	void test_ingredients_api() {
+	void test_ingredients_api_by_id() {
 		final String baseUrl = "http://localhost:8080/api/ingredient/{id}";
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<Ingredient> ingredient = 
@@ -27,6 +28,19 @@ class IngredientControllerRestTest {
 		System.out.println(ingredient.getBody());
 		
 		assertNotNull(ingredient);
+	}
+	
+	@Test
+	void test_ingredients_api() {
+		final String baseUrl = "http://localhost:8080/api/ingredient";
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<Ingredient[]> ingredients = restTemplate.getForEntity(baseUrl, Ingredient[].class);
+		
+		System.out.println(ingredients.getBody());
+		for (Ingredient t : ingredients.getBody()) {
+			System.out.println(t);
+		}
+		assertNotNull(ingredients.getBody());
 	}
 
 }
