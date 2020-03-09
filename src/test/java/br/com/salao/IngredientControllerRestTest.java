e0a7bf0678b04e70bdacc4b1189354b99a3baf12
Cristiano Carvalho Amaral <cacofut@hotmail.com>
2020-03-08 23:58:43 -0300

    Working with RestTemplate

--- src/test/java/br/com/salao/IngredientControllerRestTest.java
+++ src/test/java/br/com/salao/IngredientControllerRestTest.java
@@ -8,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import br.com.salao.entity.Ingredient;
-import br.com.salao.resource.IngredientModel;
 
 
 
@@ -36,11 +35,46 @@ class IngredientControllerRestTest {
 		RestTemplate restTemplate = new RestTemplate();
 		ResponseEntity<Ingredient[]> ingredients = restTemplate.getForEntity(baseUrl, Ingredient[].class);
 		
-		System.out.println(ingredients.getBody());
+		
 		for (Ingredient t : ingredients.getBody()) {
 			System.out.println(t);
 		}
 		assertNotNull(ingredients.getBody());
+	}
+	
+	@Test
+	void test_put_ingredient_test() {
+		final String url = "http://localhost:8080/api/ingredient";
+		RestTemplate restTemplate = new RestTemplate();
+		Ingredient ingred = new Ingredient(10L, "SRCR", "Sour cream Up", Ingredient.Type.SAUCE);
+		restTemplate.put(url, ingred, ingred.getId());
+		
+	}
+	
+	@Test
+	void test_post_ingredient_test() {
+		final String url = "http://localhost:8080/api/ingredient";
+		RestTemplate restTemplate = new RestTemplate();
+		Ingredient ingred = new Ingredient(null, "Novo", "Novo Ingredient", Ingredient.Type.VEGGIES);
+		restTemplate.postForEntity(url, ingred, Ingredient.class);
+		
+	}
+	
+	@Test
+	void test_delete_ingredient_test() {
+		final String url = "http://localhost:8080/api/ingredient/{id}";
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.delete(url, 11);
+		
+	}
+	
+	@Test
+	void test_patch_ingredient_test() {
+		final String url = "http://localhost:8080/api/ingredient/{id}";
+		RestTemplate restTemplate = new RestTemplate();
+		Ingredient ingred = new Ingredient(10L, "Novo PUT", "Novo Ingredient PUT", Ingredient.Type.WRAP);
+		restTemplate.patchForObject(url, ingred, Ingredient.class, "10");
+		
 	}
 
 }
